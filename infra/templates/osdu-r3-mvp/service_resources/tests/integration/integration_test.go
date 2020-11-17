0c4ca25a700f4d426e243acb8b1891fb2854f259
Daniel Scholl <dascholl@microsoft.com>
2020-11-17 16:17:27 -0600

    Bug Fix integration tests for service

--- infra/templates/osdu-r3-mvp/service_resources/tests/integration/integration_test.go
+++ infra/templates/osdu-r3-mvp/service_resources/tests/integration/integration_test.go
@@ -21,7 +21,6 @@ import (
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	appGatewayIntegTests "github.com/microsoft/cobalt/infra/modules/providers/azure/appgw/tests/integration"
 	redisIntegTests "github.com/microsoft/cobalt/infra/modules/providers/azure/redis-cache/tests/integration"
-	storageIntegTests "github.com/microsoft/cobalt/infra/modules/providers/azure/storage-account/tests/integration"
 	"github.com/microsoft/cobalt/test-harness/infratests"
 )
 
@@ -42,7 +41,6 @@ func TestDataEnvironment(t *testing.T) {
 		TfOptions:             tfOptions,
 		ExpectedTfOutputCount: 13,
 		TfOutputAssertions: []infratests.TerraformOutputValidation{
-			storageIntegTests.InspectStorageAccount("storage_account", "storage_containers", "services_resource_group_name"),
 			redisIntegTests.InspectProvisionedCache("redis_name", "services_resource_group_name"),
 			redisIntegTests.CheckRedisWriteOperations("redis_hostname", "redis_primary_access_key", "redis_ssl_port"),
 			appGatewayIntegTests.InspectAppGateway("services_resource_group_name", "appgw_name", "keyvault_secret_id"),
