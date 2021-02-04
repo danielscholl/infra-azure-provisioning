6d52e9c22722747c0813745d42963d5e30f30493
Daniel Scholl <dascholl@microsoft.com>
2021-02-04 14:55:59 -0600

    Bug Fix SR unit test resource count

--- infra/templates/osdu-r3-mvp/service_resources/tests/unit/unit_test.go
+++ infra/templates/osdu-r3-mvp/service_resources/tests/unit/unit_test.go
@@ -49,7 +49,7 @@ func TestTemplate(t *testing.T) {
 		TfOptions:                       tfOptions,
 		Workspace:                       workspace,
 		PlanAssertions:                  nil,
-		ExpectedResourceCount:           89,
+		ExpectedResourceCount:           91,
 		ExpectedResourceAttributeValues: resourceDescription,
 	}
 
