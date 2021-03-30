ed28b4c9157aa979ac654b52f0500026e3e4826a
vineethguna <vineethguna@microsoft.com>
2021-03-30 16:17:51 +0530

    Disabled autoscaling for airflow workers

--- charts/airflow/scripts/add-namespace.py
+++ charts/airflow/scripts/add-namespace.py
@@ -8,16 +8,11 @@ def addNamespace(namespace, manifest):
         manifest['metadata']['namespace'] = namespace
     if 'subjects' in manifest:
         manifest['subjects'][0]['namespace'] = namespace
-def removeReplicasFromWorkerStatefulSet(manifest):
-    if manifest['kind'] == 'StatefulSet' and manifest['metadata']['name'] == 'airflow-worker':
-        del manifest['spec']['replicas']
-
 
 def addingNamespace(namespace):
     for manifest in yaml.load_all(sys.stdin, Loader=yaml.FullLoader):
         if manifest:
             addNamespace(namespace, manifest)
-            removeReplicasFromWorkerStatefulSet(manifest)
             print ('---')
             print (yaml.dump(manifest, default_flow_style=False, sort_keys=False))
 
