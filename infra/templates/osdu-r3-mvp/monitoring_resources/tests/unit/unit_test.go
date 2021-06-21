ab820df61afb9621ce71e227fe6276a1c0f0dd82
Mayank Saggar <masaggar@microsoft.com>
2021-06-21 13:20:53 +0000

    Added dashboard templates for airflow infra, service and dags

--- infra/templates/osdu-r3-mvp/monitoring_resources/tests/unit/unit_test.go
+++ infra/templates/osdu-r3-mvp/monitoring_resources/tests/unit/unit_test.go
@@ -49,7 +49,7 @@ func TestTemplate(t *testing.T) {
 		TfOptions:                       tfOptions,
 		Workspace:                       workspace,
 		PlanAssertions:                  nil,
-		ExpectedResourceCount:           9,
+		ExpectedResourceCount:           12,
 		ExpectedResourceAttributeValues: resourceDescription,
 	}
 
