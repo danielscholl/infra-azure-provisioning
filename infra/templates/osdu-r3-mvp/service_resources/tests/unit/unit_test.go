f0552c84eb2fbccdd7d44f5b15bb611c1c4000bd
Abhishek Chowdhry <abhic@microsoft.com>
2021-06-21 13:27:03 +0000

    Users/abhic/adding separate redisinstance services

--- infra/templates/osdu-r3-mvp/service_resources/tests/unit/unit_test.go
+++ infra/templates/osdu-r3-mvp/service_resources/tests/unit/unit_test.go
@@ -49,7 +49,7 @@ func TestTemplate(t *testing.T) {
 		TfOptions:                       tfOptions,
 		Workspace:                       workspace,
 		PlanAssertions:                  nil,
-		ExpectedResourceCount:           107,
+		ExpectedResourceCount:           111,
 		ExpectedResourceAttributeValues: resourceDescription,
 	}
 
