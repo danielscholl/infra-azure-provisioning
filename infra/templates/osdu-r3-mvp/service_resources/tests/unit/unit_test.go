161eb79cfa49ea392326da067ee9e3fc9160aa9b
Vivek Ojha <vivekojha@microsoft.com>
2021-06-22 10:30:25 +0530

    updated unit tests expected resource count

--- infra/templates/osdu-r3-mvp/service_resources/tests/unit/unit_test.go
+++ infra/templates/osdu-r3-mvp/service_resources/tests/unit/unit_test.go
@@ -49,7 +49,7 @@ func TestTemplate(t *testing.T) {
 		TfOptions:                       tfOptions,
 		Workspace:                       workspace,
 		PlanAssertions:                  nil,
-		ExpectedResourceCount:           107,
+		ExpectedResourceCount:           108,
 		ExpectedResourceAttributeValues: resourceDescription,
 	}
 
