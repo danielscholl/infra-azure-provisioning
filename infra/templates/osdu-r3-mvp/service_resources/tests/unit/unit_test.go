77f0eff6868664615e8e83a15b17c7b45507178d
Vivek Ojha <vivekojha@microsoft.com>
2021-06-28 16:59:05 +0000

    Update unit_test.go to increase expected resources to 101

--- infra/templates/osdu-r3-mvp/service_resources/tests/unit/unit_test.go
+++ infra/templates/osdu-r3-mvp/service_resources/tests/unit/unit_test.go
@@ -49,7 +49,7 @@ func TestTemplate(t *testing.T) {
 		TfOptions:                       tfOptions,
 		Workspace:                       workspace,
 		PlanAssertions:                  nil,
-		ExpectedResourceCount:           100,
+		ExpectedResourceCount:           101,
 		ExpectedResourceAttributeValues: resourceDescription,
 	}
 
