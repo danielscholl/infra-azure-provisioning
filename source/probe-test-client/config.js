802926599e6bd4745a84b2cd6bac0313cb01896e
Aryaan Singh <arysingh@microsoft.com>
2021-08-25 03:33:17 +0000

    Changes to support probe tests service CRUD operations

--- source/probe-test-client/config.js
+++ source/probe-test-client/config.js
@@ -9,7 +9,16 @@ module.exports = {
     entitlement: `https://${process.env.OSDU_HOST}/api/entitlements/v2`,
     legal: `https://${process.env.OSDU_HOST}/api/legal/v1`,
     storage: `https://${process.env.OSDU_HOST}/api/storage/v2`,
-    search: `https://${process.env.OSDU_HOST}/api/search/v2`,
+    notification: `https://${process.env.OSDU_HOST}/api/notification/v1`,
+    partition: `https://${process.env.OSDU_HOST}/api/partition/v1`,
+    crs_catalog: `https://${process.env.OSDU_HOST}/api/crs/catalog`,
+    crs_conversion: `https://${process.env.OSDU_HOST}/api/crs/converter`,
+    ingestion_workflow: `https://${process.env.OSDU_HOST}/api/workflow/v1`,
+    policy: `https://${process.env.OSDU_HOST}/api/policy/v1`,
+    unit: `https://${process.env.OSDU_HOST}/api/unit/v3`,
+    search: `https://${process.env.OSDU_HOST}/api/search/v2/query`,
+    schema: `https://${process.env.OSDU_HOST}/api/schema-service/v1`,
+    register: `https://${process.env.OSDU_HOST}/api/register/v1`,
   },
   auth_params: {
     grant_type: "client_credentials",
@@ -20,5 +29,72 @@ module.exports = {
   telemetry_settings: {
     metrics: process.env.TRACK_METRICS,
     events: process.env.TRACK_EVENTS,
-  }
+  },
+  subscription_id: process.env.SUBSCRIPTION_ID,
+  test_flags: {
+    crud_crs_catalog:{
+      getArea: true,
+      getCatalog: true,
+      getCRS: true,
+    },
+    crud_crs_conversion: {
+      pointConversion: true,
+      geoJsonConversion: false, //400 error
+      trajectoryConversion: true,
+    },
+    crud_entitlements: {
+      enableScenario: true,
+    },
+    crud_ingestion_workflow: {
+      getAllWorkflows: true,
+      createWorkflow: true,
+      getCreatedWorkflow: true,
+    },
+    crud_legal: {
+      getAllLegalTags: true,
+      createLegalTag: true,
+      getCreatedLegalTag: true,
+    },
+    crud_notification: {
+      createSubscription: false, // 404 error
+    },
+    crud_partition: {
+      preCreateGetPartition: true,
+      createPartition: true,
+      getCreatedPartition: true,
+    },
+    crud_policy: {
+      getAllPolicies: true,
+      evaluatePolicy: false, // {"code":"resource_not_found","message":"storage_not_found_error: policy id \"test\""}
+      createTestPolicy: false, // {"code":"resource_not_found","message":"storage_not_found_error: policy id \"test\""}
+      getTestPolicy: true,
+    },
+    crud_register: {
+      subscriptionScenario: false, // Add hard coded secret in testData/sample_register_subscription_create.json
+      ddmsScenario: true,
+      actionScenario: true,
+    },
+    crud_schema: {
+      getSchemaById: true,
+      getAllSchemas: false, // 500 error
+      createSchema: false, // 400 error
+      getCreatedSchemaById: true,
+      deleteSchema: true,
+    },
+    crud_search: {
+      standardQueries: true,
+      enrichedQueries: true,
+      seismicQueries: true,
+    },
+    crud_unit: {
+      getUnits: true,
+      getCatalog: true,
+      getMeasurements: true,
+      getUnitMaps: true,
+      getUnitSystems: true,
+    },
+    scenario_searchInsertedRecord: {
+      enableScenario: true,
+    },
+  },
 };
---
+++ source/probe-test-client/scenarios/crud_legal.js
@@ -0,0 +1,175 @@
+'use strict';
+
+// Imports
+const should = require('chai').Should();
+const { expect } = require('chai');
+const request = require("supertest");
+const config = require(`${__dirname}/../config`);
+const testUtils = require(`${__dirname}/../utils/testUtils`);
+const telemetryUtils = require(`${__dirname}/../utils/telemetryUtils`);
+const { assert } = require('console');
+var isEqual = require('lodash.isequal');
+
+// Test Setup
+let scenario = "legalservice_crudOps";
+
+const majorVersion = testUtils.between(1, 100000);
+const minorVersion = testUtils.between(1, 100000);
+let runId = `${majorVersion}.${minorVersion}`;
+
+console.log(`run ID: ${runId}`);
+
+let test = {
+  runId: runId,
+  scenario: scenario,
+  service: "service",
+  api: "api",
+  expectedResponse: -1,
+  failedTests: 0,
+};
+
+// Test Data Setup
+let tag = 'opendes-public-usa-dataset-1'
+const sampleLegalTag = require(`${__dirname}/../testData/sample_legal_tag_crud.json`);
+sampleLegalTag.name = tag
+let data = "";
+
+// Test Scenario
+describe(scenario, (done) => {
+    let token;
+
+    before(done => {
+        // Get a new OAuth Token
+        testUtils.oAuth.post('/token')
+          .set('Content-Type', 'application/x-www-form-urlencoded')
+          .send(config.auth_params)
+          .then(res => {
+            token = 'Bearer ' + res.body.access_token;
+            done();
+          })
+          .catch(err => {
+            done(err)
+          });
+    });
+
+    describe('Scenario: Legal Service supports CRUD operations', (done) => {
+
+        if (config.test_flags.crud_legal.getAllLegalTags) {
+            it("Get All Legal Tags", done => {
+                test.service = testUtils.services.legal;
+                test.api = test.service.api.getAllLegalTags;
+                test.expectedResponse = test.api.expectedResponse;
+                
+                test.service.host
+                .get(test.api.path)
+                .set('Authorization', token)
+                .set('data-partition-id', testUtils.partition)
+                .expect(test.expectedResponse)
+                .then(res => {
+                    telemetryUtils.passApiRequest(test); 
+                    done(); 
+                })
+                .catch(err => { 
+                    telemetryUtils.failApiRequest(test, err);
+                    done(err); 
+                });
+            });
+        }
+
+        if (config.test_flags.crud_legal.createLegalTag) {
+            it("Create Legal Tag", done => {
+                test.service = testUtils.services.legal;
+                test.api = test.service.api.createLegalTags;
+                test.expectedResponse = test.api.expectedResponse;
+                
+                test.service.host
+                .post(test.api.path)
+                .set('Authorization', token)
+                .set('data-partition-id', testUtils.partition)
+                .send(sampleLegalTag)
+                .then(res => {
+                    test.expectedResponse.should.be.an('array').that.includes(res.statusCode);
+                    telemetryUtils.passApiRequest(test);
+                    done();
+                })
+                .catch(err => {
+                    telemetryUtils.failApiRequest(test, err);
+                    done(err)
+                });
+            });
+    
+            if (config.test_flags.crud_legal.getCreatedLegalTag) {
+                it("Get Created Legal Tag", done => {
+                    test.service = testUtils.services.legal;
+                    test.api = test.service.api.getLegalTag;
+                    test.expectedResponse = test.api.expectedResponse;
+                    
+                    test.service.host
+                    .get(test.api.path + sampleLegalTag.name)
+                    .set('Authorization', token)
+                    .set('data-partition-id', testUtils.partition)
+                    .expect(test.expectedResponse)
+                    .then(res => {
+                        data = res.body;
+                        telemetryUtils.passApiRequest(test); 
+                        done(); 
+                    })
+                    .catch(err => { 
+                        telemetryUtils.failApiRequest(test, err);
+                        done(err); 
+                    });
+                });
+        
+                it('Validate: Get Created Legal Tag', () => {
+                    test.service = testUtils.services.legal;
+                    test.api.name = 'validate_' + test.service.api.getLegalTag.name;
+                    test.expectedResponse = "Valid Match";
+        
+                    if (isEqual(data, sampleLegalTag)) {
+                        telemetryUtils.passApiRequest(test);
+                        expect(true).to.be.true;
+                    } else {
+                        telemetryUtils.failApiRequest(test, "Invalid");
+                            console.log(`actual:${JSON.stringify(data)}`);
+                        expect(false).to.be.true;
+                    }
+                });
+            }
+    
+            describe('Probe Cleanup', done => {
+    
+                it('Delete Created LegalTag', done => {
+                    test.service = testUtils.services.legal;
+                    test.api = test.service.api.deleteLegalTags;
+                    test.expectedResponse = test.api.expectedResponse;
+    
+                    test.service.host
+                    .delete(test.api.path + sampleLegalTag.name)
+                    .set('Authorization', token)
+                    .set('Accept', 'application/json')
+                    .set('data-partition-id', testUtils.partition)
+                    .expect(test.expectedResponse)
+                    .then(() => {
+                        telemetryUtils.passApiRequest(test); 
+                        done(); 
+                    })
+                    .catch(err => { 
+                        telemetryUtils.failApiRequest(test, err);
+                        done(err); 
+                    });
+                });
+            });
+        }
+        
+        afterEach(function() {
+            if (this.currentTest.state === 'failed') {
+                test.failedTests += 1;
+            }
+          });
+
+        after(done => {
+            telemetryUtils.scenarioRequest(runId, testUtils.services.legal, scenario, test.failedTests);
+            done();
+        });
+    });
+});
