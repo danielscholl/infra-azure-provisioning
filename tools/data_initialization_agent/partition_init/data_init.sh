e54b1192c77c052a9e717172df88d902dabbbf75
Aryaan Singh <arysingh@microsoft.com>
2021-08-31 14:58:53 +0000

    Add Config Map Modifications To Data Agents

--- tools/data_initialization_agent/partition_init/data_init.sh
+++ tools/data_initialization_agent/partition_init/data_init.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+currentStatus=""
+currentMessage=""
+
+az login --identity
+ENV_AKS=$(az aks list --resource-group $RESOURCE_GROUP_NAME --query [].name -otsv)
+az aks get-credentials --resource-group $RESOURCE_GROUP_NAME --name $ENV_AKS
+kubectl config set-context $RESOURCE_GROUP_NAME --cluster $ENV_AKS
+
 OSDU_URI=${OSDU_HOST}
 
 if [[ ${OSDU_HOST} != "https://"* ]] || [[ ${OSDU_HOST} != "http://"* ]]; then
@@ -9,80 +17,118 @@ fi
 echo "Trying to Fetch Access Token"
 ACCESS_TOKEN=$(sh ./get_access_token.sh)
 if [[ "$ACCESS_TOKEN" == "TOKEN_FETCH_FAILURE" ]]; then
-  echo "Failure fetching Access Token"
-  exit 1
-fi
-echo "Access Token fetched successfully."
+  currentStatus="failure"
+  currentMessage="${currentMessage}. Failure fetching Access Token. "
+else
+  echo "Access Token fetched successfully."
 
-IFS=',' read -r -a partitions_array <<< ${PARTITIONS}
-
-partition_count=0
-partition_initialized_count=0
-
-for index in "${!partitions_array[@]}"
-do
-  partition_count=$(expr $partition_count + 1)
-  echo "Intitializing Partition: ${partitions_array[index]}"
-
-  OSDU_PARTITION_INIT_URI=${OSDU_URI}/api/partition/v1/partitions/${partitions_array[index]}
-  echo "Partition Initialization Endpoint: ${OSDU_PARTITION_INIT_URI}"
-
-  i=0
-  while [[ $i -lt 3 ]]; do
-    i=$(expr $i + 1)
-
-    init_response=$(curl -s -w " Http_Status_Code:%{http_code} " \
-      -X POST \
-      -H 'Content-Type: application/json' \
-      -H "Authorization: Bearer $ACCESS_TOKEN" \
-      -H "data-partition-id: ${partitions_array[index]}" \
-      -d "@partition_init_api_payload.json" \
-      $OSDU_PARTITION_INIT_URI)
+  IFS=',' read -r -a partitions_array <<< ${PARTITIONS}
   
-    echo "Init Reponse: $init_response"
-
-    if [ -z "$init_response" -a "$init_response"==" " ]; then
-      echo "Initialization Failed, Empty Reponse. Iteration $i."
-      continue
-    fi
-
-    # Status code check. succeed only if 2xx
-    # quit for partition if 404 or 400.
-    # 401 or 403, then retry after getting access token.
-    # else sleep for 1min and retry
-    if [[ ${init_response} != *"Http_Status_Code:2"* ]];then
-      if [[ ${init_response} == *"Http_Status_Code:400"* ]] || [[ ${init_response} == *"Http_Status_Code:404"* ]];then
-        echo "Partition Init for partition ${partitions_array[index]} failed with response $init_response"
+  partition_count=0
+  partition_initialized_count=0
+  
+  for index in "${!partitions_array[@]}"
+  do
+    partition_count=$(expr $partition_count + 1)
+    echo "Intitializing Partition: ${partitions_array[index]}"
+  
+    OSDU_PARTITION_INIT_URI=${OSDU_URI}/api/partition/v1/partitions/${partitions_array[index]}
+    echo "Partition Initialization Endpoint: ${OSDU_PARTITION_INIT_URI}"
+  
+    i=0
+    while [[ $i -lt 3 ]]; do
+  
+      init_response=$(curl -s -w " Http_Status_Code:%{http_code} " \
+        -X POST \
+        -H 'Content-Type: application/json' \
+        -H "Authorization: Bearer $ACCESS_TOKEN" \
+        -H "data-partition-id: ${partitions_array[index]}" \
+        -d "@partition_init_api_payload.json" \
+        $OSDU_PARTITION_INIT_URI)
+    
+      echo "Init Reponse: $init_response"
+  
+      if [ -z "$init_response" -a "$init_response"==" " ]; then
+        echo "Initialization Failed, Empty Reponse. Iteration $i."
+        continue
+      fi
+  
+      # Status code check. succeed only if 2xx
+      # quit for partition if 404 or 400.
+      # 401 or 403, then retry after getting access token.
+      # else sleep for 1min and retry
+      if [[ ${init_response} != *"Http_Status_Code:2"* ]];then
+        if [[ ${init_response} == *"Http_Status_Code:400"* ]] || [[ ${init_response} == *"Http_Status_Code:404"* ]];then
+          currentStatus="failure"
+          currentMessage="${currentMessage}. Partition Init for partition ${partitions_array[index]} failed with response $init_response. "
+          echo "Partition Init for partition ${partitions_array[index]} failed with response $init_response"
+          break
+        fi
+  
+        echo "Sleeping for 1min."
+        sleep 1m
+  
+        if [[ ${init_response} == *"Http_Status_Code:401"* ]] || [[ ${init_response} == *"Http_Status_Code:403"* ]];then
+          echo "Trying to Re-Fetch Access Token"
+          ACCESS_TOKEN=$(sh ./get_access_token.sh)
+          if [[ "$ACCESS_TOKEN" == "TOKEN_FETCH_FAILURE" ]]; then
+            currentStatus="failure"
+            currentMessage="${currentMessage}. Failure re-fetching Access Token. "
+            echo "Failure re-fetching Access Token"
+            break
+          fi
+          echo "Access Token re-fetched successfully."
+        fi
+  
+        continue
+      else
+        currentMessage="${currentMessage}. Partition ${partitions_array[index]} Initialized successfully. "
+        echo "Partition ${partitions_array[index]} Initialized successfully."
+        partition_initialized_count=$(expr $partition_initialized_count + 1)
+  
         break
       fi
+      
+      i=$(expr $i + 1)
+    done
 
-      echo "Sleeping for 1min."
-      sleep 1m
-
-      if [[ ${init_response} == *"Http_Status_Code:401"* ]] || [[ ${init_response} == *"Http_Status_Code:403"* ]];then
-        echo "Trying to Re-Fetch Access Token"
-        ACCESS_TOKEN=$(sh ./get_access_token.sh)
-        if [[ "$ACCESS_TOKEN" == "TOKEN_FETCH_FAILURE" ]]; then
-          echo "Failure re-fetching Access Token"
-          exit 1
-        fi
-        echo "Access Token re-fetched successfully."
-      fi
-
-      continue
-    else
-      echo "Partition ${partitions_array[index]} Initialized successfully."
-      partition_initialized_count=$(expr $partition_initialized_count + 1)
-
-      break
+    if [[ $i -ge 3 ]]; then
+      currentStatus="failure"
+      currentMessage="${currentMessage}. Max Number of retries reached. "
     fi
   done
-done
+  
+  if [ "$partition_count" -ne "$partition_initialized_count" ]; then
+    currentStatus="failure"
+    currentMessage="${currentMessage}. $partition_initialized_count partition(s) of total $partition_count partition(s) initialized successfully. "
+    echo "$partition_initialized_count partition(s) of total $partition_count partition(s) initialized successfully."
+  else
+    currentMessage="${currentMessage}. All $partition_initialized_count partition(s) initialized successfully. "
+    echo "All $partition_initialized_count partition(s) initialized successfully."
+  fi
+fi
 
-if [ "$partition_count" -ne "$partition_initialized_count" ]; then
-  echo "$partition_initialized_count partition(s) of total $partition_count partition(s) initialized successfully."
-  exit 1
+if [ -z "$currentStatus" -a "$currentStatus"==" " ]; then
+    currentStatus="success"
+fi
+echo "Current Status: ${currentStatus}"
+echo "Current Message: ${currentMessage}"
+
+if [ ! -z "$CONFIG_MAP_NAME" -a "$CONFIG_MAP_NAME" != " " ]; then
+    Status=$(kubectl get configmap $CONFIG_MAP_NAME -o jsonpath='{.data.status}')
+    Message=$(kubectl get configmap $CONFIG_MAP_NAME -o jsonpath='{.data.message}')
+
+    if [[ ${Status} == *"success"* ]]; then # If status is already failed, do not over-write in any case.
+        Status="${currentStatus}"
+    fi
+    Message="${Message}. Partition Data Initialization: ${currentMessage}. "
+
+    ## Update ConfigMap
+    kubectl create configmap $CONFIG_MAP_NAME --from-literal=status="$Status" --from-literal=message="$Message" -o yaml --dry-run=client | kubectl replace -f -
+fi
+
+if [[ ${currentStatus} == "success" ]]; then
+    exit 0
 else
- echo "All $partition_initialized_count partition(s) initialized successfully."
- exit 0
+    exit 1
 fi
