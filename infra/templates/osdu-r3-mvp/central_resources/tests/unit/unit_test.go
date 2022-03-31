82836a43147f675842434ee3420ac52a3cf7ea24
unknown <aayushi.jain@microsfot.com>
2022-03-31 10:39:59 +0530

    Fixing UT

--- infra/templates/osdu-r3-mvp/central_resources/tests/unit/unit_test.go
+++ infra/templates/osdu-r3-mvp/central_resources/tests/unit/unit_test.go
@@ -49,7 +49,7 @@ func TestTemplate(t *testing.T) {
 		TfOptions:                       tfOptions,
 		Workspace:                       workspace,
 		PlanAssertions:                  nil,
-		ExpectedResourceCount:           58,
+		ExpectedResourceCount:           59,
 		ExpectedResourceAttributeValues: resourceDescription,
 	}
 
